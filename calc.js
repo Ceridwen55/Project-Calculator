1a5ee7b997fb5a3b9ed3d0dad582db2bee1881ab
Zaqzulkan <zulkanzaqzuq@gmail.com>
2024-08-17 20:14:17 +0700

    Almost finished with the styling

--- calc.js
+++ calc.js
@@ -6,9 +6,15 @@ let operator = null;
 //DISPLAY
 const display = document.querySelector(".display");
 display.style.display = "flex";
-display.style.justifyContent = "flex-end"
-display.style.width = "100px";
-display.style.height = "35px";
+display.style.justifyContent = "flex-end";
+display.style.alignItems = "center";
+display.style.flexWrap = "wrap";
+display.style.width = "200px";
+display.style.height = "50px";
+display.style.fontWeight = "bold";
+display.style.border = "solid";
+display.style.backgroundColor = "#4CAF50";
+display.style.fontSize = "21px";
 display.textContent = 0;
 function print (value)
 {
@@ -61,6 +67,10 @@ const mechanism = input.forEach(item => // CLICK AND SHOW VALUE IN PREVIOUS FUNC
 
 let equal = document.getElementById("19");
 equal.addEventListener('click', calculateResult);
+equal.style.display = "flex";
+equal.style.height = "50px";
+equal.style.width = "50px";
+equal.style.border = "solid";
 
 let plus = document.getElementById("16");
 function add ()
@@ -68,6 +78,10 @@ function add ()
     operator = '+';
 }
 plus.addEventListener('click',add)
+plus.style.display = "flex";
+plus.style.height = "50px";
+plus.style.width = "50px";
+plus.style.border = "solid";
 
 let minus = document.getElementById("12");
 function substract ()
@@ -75,6 +89,10 @@ function substract ()
     operator = '-';
 }
 minus.addEventListener('click', substract);
+minus.style.display = "flex";
+minus.style.height = "50px";
+minus.style.width = "50px";
+minus.style.border = "solid";
 
 let x = document.getElementById("8");
 function multiply()
@@ -83,6 +101,15 @@ function multiply()
     operator = '*';
 }
 x.addEventListener('click', multiply);
+x.style.display = "flex";
+x.style.height = "50px";
+x.style.width = "50px";
+x.style.fontWeight = "bold";
+x.style.justifyContent = "center";
+x.style.alignItems = "center";
+x.style.fontStyle = "normal";
+x.style.fontSize = "16px";
+x.style.border = "solid";
 
 let div = document.getElementById("4");
 function divide ()
@@ -90,6 +117,15 @@ function divide ()
     operator = '/';
 }
 div.addEventListener('click', divide);
+div.style.display = "flex";
+div.style.height = "50px";
+div.style.width = "50px";
+div.style.fontWeight = "bold";
+div.style.justifyContent = "center";
+div.style.alignItems = "center";
+div.style.fontStyle = "normal";
+div.style.fontSize = "16px";
+div.style.border = "solid";
 
 let rem = document.getElementById("3");
 function remainder ()
@@ -97,6 +133,15 @@ function remainder ()
     operator = '%';
 }
 rem.addEventListener('click', remainder);
+rem.style.display = "flex";
+rem.style.height = "50px";
+rem.style.width = "50px";
+rem.style.fontWeight = "bold";
+rem.style.justifyContent = "center";
+rem.style.alignItems = "center";
+rem.style.fontStyle = "normal";
+rem.style.fontSize = "16px";
+rem.style.border = "solid";
 
 let zero = document.getElementById("1");
 function reset ()
@@ -106,6 +151,15 @@ function reset ()
     print(0);
 }
 zero.addEventListener('click', reset);
+zero.style.display = "flex";
+zero.style.height = "50px";
+zero.style.width = "50px";
+zero.style.fontWeight = "bold";
+zero.style.justifyContent = "center";
+zero.style.alignItems = "center";
+zero.style.fontStyle = "normal";
+zero.style.fontSize = "16px";
+zero.style.border = "solid";
 
 let change = document.getElementById("2");
 function mirror ()
@@ -124,6 +178,15 @@ function mirror ()
     
 }
 change.addEventListener('click',mirror);
+change.style.display = "flex";
+change.style.height = "50px";
+change.style.width = "50px";
+change.style.fontWeight = "bold";
+change.style.justifyContent = "center";
+change.style.alignItems = "center";
+change.style.fontStyle = "normal";
+change.style.fontSize = "16px";
+change.style.border = "solid";
 
 let comma = document.getElementById("18");
 function decimal ()
@@ -147,6 +210,15 @@ function decimal ()
 }
 
 comma.addEventListener('click',decimal);
+comma.style.display = "flex";
+comma.style.height = "50px";
+comma.style.width = "50px";
+comma.style.fontWeight = "bold";
+comma.style.justifyContent = "center";
+comma.style.alignItems = "center";
+comma.style.fontStyle = "normal";
+comma.style.fontSize = "16px";
+comma.style.border = "solid";
 
 
 
@@ -202,53 +274,60 @@ rex.style.alignItems = "center";
 let maiden = document.querySelector(".queen");
 maiden.style.display = "flex";
 maiden.style.flexDirection = "column";
-maiden.style.gap = "5px";
-maiden.style.marginTop = "350px";
-maiden.style.marginLeft = "150px";
+maiden.style.backgroundColor = "#003366"
+maiden.style.gap = "2px";
+maiden.style.marginTop = "300px";
+maiden.style.marginLeft = "50px";
 maiden.style.height = "200x";
 maiden.style.width = "200px";
+maiden.style.border = "solid";
 
 let firstr = document.querySelector(".firstrow");
 firstr.style.display = "flex";
-firstr.style.width = "500px";
-firstr.style.height = "100px";
+firstr.style.width = "200px";
+firstr.style.height = "50px";
+firstr.style.gap = "2px";
 
 let secondr = document.querySelector(".secondrow");
 secondr.style.display = "flex";
 secondr.style.width = "200px"
 secondr.style.height = "50px"
-secondr.style.gap ="5px";
+secondr.style.gap ="2px";
 
 let thirdr = document.querySelector(".thirdrow");
 thirdr.style.display = "flex";
 thirdr.style.width = "200px"
 thirdr.style.height = "50px"
-thirdr.style.gap ="5px";
+thirdr.style.gap ="2px";
 
 let fourthr = document.querySelector(".fourthrow");
 fourthr.style.display = "flex";
 fourthr.style.width = "200px"
 fourthr.style.height = "50px"
-fourthr.style.gap ="5px";
+fourthr.style.gap ="2px";
 
 let fifthr = document.querySelector(".fifthrow");
 fifthr.style.display = "flex";
 fifthr.style.width = "200px"
 fifthr.style.height = "50px"
-fifthr.style.gap ="5px";
+fifthr.style.gap ="2px";
 
 
 input.forEach(item => 
 {
-    if (item.but)
-    {
+    
         item.but.style.display = "flex";
         item.but.style.width = "50px";
         item.but.style.height = "50px";
+        item.but.style.fontWeight = "bold";
+        item.but.style.justifyContent = "center";
+        item.but.style.alignItems = "center";
+        item.but.style.fontStyle = "oblique";
+        item.but.style. fontSize = "14px";
+        item.but.style.border = "solid";
         input[0].but.style.width = "105px";
-    }
-}
-)
+    
+});
 
 
 
